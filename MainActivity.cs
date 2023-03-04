9d108d199125eaf128715ba400e858c9968f90b4
Brayan Hincapie Monsalve <monslaveb@gmail.com>
2023-03-04 18:55:44 -0500

    vers 2

--- MainActivity.cs
+++ MainActivity.cs
@@ -3,12 +3,10 @@ using Android.Content;
 using Android.Content.Res;
 using Android.OS;
 using Android.Runtime;
-using Android.Views;
 using Android.Widget;
 using AndroidX.AppCompat.App;
 using System;
 using Google.Android.Material.BottomNavigation;
-using Android.Views;
 
 
 
@@ -25,6 +23,15 @@ namespace Entrega
         Button signUpButton;
         Button loginButton;
 
+        //PQR
+        Button btnSend;
+        EditText txtFullName;
+        EditText txtPhone;
+        EditText txtId;
+        EditText txtQuestion;
+        Spinner spinnerPqr;
+
+
         // Button of sign up
         Button btnBack;
         Button btnSUSignUp;
@@ -37,6 +44,19 @@ namespace Entrega
         EditText txtSUSuerName;
         EditText txtSUPassword;
         EditText txtSUCPassword;
+        EditText txtSUEmail;
+
+        //Forgor password
+
+        EditText txtFUserName;
+        EditText txtFEmail;
+        EditText txtFCEmail;
+        Button btnFSend;
+
+        Button btnForgot;
+
+        Button btnFBack;
+
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -52,7 +72,29 @@ namespace Entrega
             //Validate login form
             loginButton = FindViewById<Button>(Resource.Id.btnLogin);
             loginButton.Click += LoginLayout;
+
+            //Forgot paswword
+            btnForgot = FindViewById<Button>(Resource.Id.btnForgot);
+            btnForgot.Click += forgotPassword;
+
+
         }
+        private void forgotPassword(object sender, EventArgs e)
+        {
+            SetContentView(Resource.Layout.forgotPass);
+            //Bact to main
+            btnFBack = FindViewById<Button>(Resource.Id.btnFBack);
+            btnFBack.Click += delegate
+            {
+                Intent intent = new Intent(this, typeof(MainActivity));
+                StartActivity(intent);
+            };
+
+            btnFSend = FindViewById<Button>(Resource.Id.btnFSend);
+            btnFSend.Click += ForgotBtnFSend;
+
+        }
+
         private void LoginLayout(object sender, EventArgs e)
         {
             txtPassword = FindViewById<EditText>(Resource.Id.txtPassword);
@@ -64,40 +106,108 @@ namespace Entrega
             else
             {
                 SetContentView(Resource.Layout.welcome);
-                BottomNavigationView navigationMenu = FindViewById<BottomNavigationView>(Resource.Id.navigation);
-                navigationMenu.NavigationItemSelected += (sender, e) =>
-                {
-
-                    
-                    switch (e.Item.ItemId)
-                    {
-                        case Resource.Id.idPQR:
-                            // El elemento de inicio ha sido seleccionado
-                            pqr();
-                            break;
-
-                        case Resource.Id.navigation_dashboard:
-                            // El elemento de dashboard ha sido seleccionado
-                            break;
-
-                    }
-                };
-
-
+                menu();
             }
 
         }
+
+        private void menu()
+        {
+            BottomNavigationView navigationMenu = FindViewById<BottomNavigationView>(Resource.Id.navigation);
+            navigationMenu.NavigationItemSelected += (sender, e) =>
+            {
+
+
+                switch (e.Item.ItemId)
+                {
+                    case Resource.Id.idPQR:
+                        pqr();
+                        break;
+
+                    case Resource.Id.home:
+                        welcome();
+                        break;
+
+                    case Resource.Id.exitId:
+                        exit();
+                        break;
+
+                }
+            };
+
+        }
+
+        private void welcome()
+        {
+            SetContentView(Resource.Layout.welcome);
+            menu();
+        }
+
         private void pqr()
         {
             SetContentView(Resource.Layout.pqr);
+            menu();
+            btnSend = FindViewById<Button>(Resource.Id.btnSend);
+            btnSend.Click += (sender, e) =>
+            {
+
+                txtFullName = FindViewById<EditText>(Resource.Id.txtFullName);
+                txtId = FindViewById<EditText>(Resource.Id.txtId);
+                txtPhone = FindViewById<EditText>(Resource.Id.txtPhone);
+                txtQuestion = FindViewById<EditText>(Resource.Id.txtQuestion);
+                spinnerPqr = FindViewById<Spinner>(Resource.Id.spinnerPqr);
+
+                if (string.IsNullOrEmpty(txtFullName.Text) || string.IsNullOrEmpty(txtId.Text) || string.IsNullOrEmpty(txtPhone.Text) || string.IsNullOrEmpty(txtQuestion.Text) || spinnerPqr.SelectedItem.ToString() == "Seleccione")
+                {
+                    ShowAlertDialog("Error", "Please complete all fields", "OK");
+                }
+                else
+                {
+                    txtFullName.Text = "";
+                    txtId.Text = "";
+                    txtPhone.Text = "";
+                    txtQuestion.Text = "";
+                    //spinnerPqr.SelectedItemPosition = -1;
+                    ShowAlertDialog("Success", "PQR sent Successfully", "OK");
+
+                }
+
+            };
+                
+        }
+        private void exit()
+        {
+            Intent intent = new Intent(this, typeof(MainActivity));
+            StartActivity(intent);
         }
 
 
-        //Inflar el menu
-        public override bool OnCreateOptionsMenu(IMenu menu)
+
+        private void ForgotBtnFSend(object sender, EventArgs e)
         {
-            MenuInflater.Inflate(Resource.Menu.navigation, menu);
-            return true;
+            
+            txtFCEmail = FindViewById<EditText>(Resource.Id.txtFCEmail);
+            txtFEmail = FindViewById<EditText>(Resource.Id.txtFEmail);
+            txtFUserName = FindViewById<EditText>(Resource.Id.txtFUserName);
+
+            if (string.IsNullOrEmpty(txtFUserName.Text) || string.IsNullOrEmpty(txtFCEmail.Text) || string.IsNullOrEmpty(txtFEmail.Text))
+            {
+                ShowAlertDialog("Error", "Please enter a valid user data", "OK");
+            }
+            else
+            {
+                if (txtFCEmail.Text == txtFEmail.Text)
+                {
+                    Intent intent = new Intent(this, typeof(MainActivity));
+                    StartActivity(intent);
+                    ShowAlertDialog("Success", "Password sent your email", "OK");
+                }
+                else
+                {
+                    ShowAlertDialog("Error", "Passwords do not match", "OK");
+                }
+            }
+            
         }
 
 
@@ -124,10 +234,15 @@ namespace Entrega
             txtSUSuerName = FindViewById<EditText>(Resource.Id.txtSUSuerName);
             txtSUPassword = FindViewById<EditText>(Resource.Id.txtSUPassword);
             txtSUCPassword = FindViewById<EditText>(Resource.Id.txtSUCPassword);
+            txtSUEmail = FindViewById<EditText>(Resource.Id.txtSUEmail);
 
             if (string.IsNullOrEmpty(txtSUSuerName.Text))
             {
                 ShowAlertDialog("Error", "Please enter a valid user name", "OK");
+            }
+            else if(string.IsNullOrEmpty(txtSUEmail.Text))
+            {
+                ShowAlertDialog("Error", "Please enter a valid user email", "OK");
             }
             else if (string.IsNullOrEmpty(txtSUPassword.Text))
             {
